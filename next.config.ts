47c2bb8f15f4be6599d40cfa7d0da4ac841a9c20
fjordimm <fjordimm@gmail.com>
2026-01-18 17:46:50 -0800

    reversed broken paths for public files

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
     output: "export",
     images: {
         unoptimized: true
-    },
-    basePath: process.env.NODE_ENV === "production" ? "/your-repo-name" : "",
-    assetPrefix: process.env.NODE_ENV === "production" ? "/your-repo-name/" : ""
+    }
 };
 
 export default nextConfig;
