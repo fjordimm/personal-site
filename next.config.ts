d955d982ac068f28a10e7e5afe5b0b12e017da11
fjordimm <fjordimm@gmail.com>
2026-01-18 17:27:19 -0800

    made github workflow for pages; changed formatting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    images: {
+        unoptimized: true
+    },
+    basePath: "/personal-site",
+    assetPrefix: "personal-site/"
 };
 
 export default nextConfig;
