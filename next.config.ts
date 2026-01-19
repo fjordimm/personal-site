c5c19de21fee6e0ba11ec0628b0c6ea236f37ada
fjordimm <fjordimm@gmail.com>
2026-01-18 17:37:41 -0800

    fixed paths not working for production vs development

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
     output: "export",
     images: {
         unoptimized: true
-    }
+    },
+    basePath: process.env.NODE_ENV === "production" ? "/your-repo-name" : "",
+    assetPrefix: process.env.NODE_ENV === "production" ? "/your-repo-name/" : ""
 };
 
 export default nextConfig;
