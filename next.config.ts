e1641b3bd393e5885a71a124673e4e5ff707c1c1
fjordimm <fjordimm@gmail.com>
2026-01-22 11:27:52 -0800

    fixing resource paths

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,7 @@ const nextConfig: NextConfig = {
         unoptimized: true
     },
     basePath: process.env.NODE_ENV === 'production' ? '/personal-site' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-site/' : '',
-    trailingSlash: true
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-site/' : ''
 };
 
 export default nextConfig;
