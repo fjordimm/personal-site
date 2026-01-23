5ece18b682e2e328a5c46a4819d984f9facd2629
fjordimm <fjordimm@gmail.com>
2026-01-22 16:11:55 -0800

    small change

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     output: "export",
     images: {
         unoptimized: true
-    }
+    },
     // basePath: process.env.NODE_ENV === 'production' ? '/personal-site' : '',
     // assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-site/' : ''
 };
