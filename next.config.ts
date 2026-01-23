27c10db3f865dd48fbb4981df8fe5ba94fd67f18
fjordimm <fjordimm@gmail.com>
2026-01-22 16:08:12 -0800

    redoing github workflow

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    basePath: "/personal-site",
     output: "export",
     images: {
         unoptimized: true
-    },
-    basePath: process.env.NODE_ENV === 'production' ? '/personal-site' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-site/' : ''
+    }
+    // basePath: process.env.NODE_ENV === 'production' ? '/personal-site' : '',
+    // assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-site/' : ''
 };
 
 export default nextConfig;
