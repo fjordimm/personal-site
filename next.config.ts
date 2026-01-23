9ad538b4b42c9c1331f222f80b65ec24b0000b4e
fjordimm <fjordimm@gmail.com>
2026-01-22 17:13:37 -0800

    redid layout of stuff

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    basePath: "/personal-site",
+    // basePath: "/personal-site",
     output: "export",
     images: {
         unoptimized: true
-    },
-    // basePath: process.env.NODE_ENV === 'production' ? '/personal-site' : '',
-    // assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-site/' : ''
+    }
 };
 
 export default nextConfig;
